0b902c4d80618733a57823325ee359aba996a645
ilyas <elaissiilyas@gmail.com>
2023-06-03 15:57:25 +0100

    Confirm email html

--- backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/service/email/EmailServiceImpl.java
+++ backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/service/email/EmailServiceImpl.java
@@ -1,10 +1,15 @@
 package me.ilyaselaissi.linechatapi.service.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Slf4j
 @Service
@@ -20,14 +25,37 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendConfirmationEmail(String email, String token) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreplay@linechat.chat");
-        message.setTo(email);
-        message.setSubject("Email Confirmation");
-        message.setText("Please confirm your email address by clicking the link: "
-                + FRONTEND_URL + "confirm-email?token=" + token
-        );
         try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom("noreply@linechat.chat");
+            helper.setTo(email);
+            helper.setSubject("LineChat - Confirm your email address");
+
+            // Create the HTML content with a link
+            String confirmationLink = FRONTEND_URL + "/confirm-email?token=" + token;
+            String htmlContent = "<html>"
+                    + "<head>"
+                    + "<style>"
+                    + "h1 { color: #333; }"
+                    + "p { color: #666; }"
+                    + "a { color: #007bff; text-decoration: none; }"
+                    + "</style>"
+                    + "</head>"
+                    + "<body>"
+                    + "<h1>LineChat</h1>"
+                    + "<p>Thank you for registering with LineChat. Please click on the link below to confirm your email address:</p>"
+                    + "<p>Please click <a href=\"" + confirmationLink + "\">here</a> to confirm your email.</p>"
+                    + "<p>If the hyperlink doesn't work, you can copy and paste the following URL into your browser:</p>"
+                    + "<p>" + confirmationLink + "</p>"
+                    + "</body>"
+                    + "</html>";
+            helper.setText(htmlContent, true);
+
+            // Set the Date
+            helper.setSentDate(new Date());
+
             mailSender.send(message);
             log.info("Email sent to: " + email);
         } catch (Exception e) {
