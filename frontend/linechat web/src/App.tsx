7e3d460ffde92a0baa2a763859d5ead7d5aefccc
ilyas <elaissiilyas@gmail.com>
2023-06-06 18:55:39 +0100

    adding pages

--- frontend/linechat web/src/App.tsx
+++ frontend/linechat web/src/App.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom' 
 import Index from './pages/Index'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/">
     <Route index element={<Index />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
     <Route path="dashboard" element={<Dashboard />} />
   </Route>,
 ))
