8f550565909a5d5533314c314c448e038b9ced0d
ilyas <elaissiilyas@gmail.com>
2023-06-09 16:04:45 +0100

    dashboard page

--- frontend/linechat web/src/pages/Dashboard.tsx
+++ frontend/linechat web/src/pages/Dashboard.tsx
@@ -1,10 +1,22 @@
+import { useAuthorize } from '../hooks/useAuthorize'
+import { useSignOut } from '../hooks/useSignOut'
+import useTitle from '../hooks/useTitle'
 import { useAuthStore } from '../stores/authStore'
 
 const Dashboard = () => {
+  useAuthorize()
+  useTitle('Dashboard')
+  const signOut = useSignOut()
   const bearerToken = useAuthStore((state) => state.bearerToken)
+  if (!bearerToken) {
+    return null
+  }
+  
   return (
-    <div>Dashboard
+    <div>
+      Dashboard
       <p>{bearerToken}</p>
+      <button onClick={signOut}>Logout</button>
     </div>
   )
 }
