554b0f3444fe7838834b2d0d524899aed66955c8
ilyas <elaissiilyas@gmail.com>
2023-06-11 13:56:02 +0100

    adding dark mode

--- frontend/linechat web/src/pages/Dashboard.tsx
+++ frontend/linechat web/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useAuthorize } from '../hooks/useAuthorize'
 import { useSignOut } from '../hooks/useSignOut'
 import useTitle from '../hooks/useTitle'
@@ -8,17 +9,30 @@ const Dashboard = () => {
   useAuthorize()
   const signOut = useSignOut()
   const bearerToken = useAuthStore((state) => state.bearerToken)
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('dark')
+    }
+  }, [])
   if (!bearerToken) {
     return null
   }
+
   
+  const handleDarkMode = () => {
+    document.documentElement.classList.toggle('dark')
+  }
+  
+
   return (
-    <div className='bg-primary h-screen max-h-screen w-full overflow-hidden flex'>
-      <div className='fixed bottom-0 lg:static lg:bottom-none bg-white border-t border-gray-200 lg:border-0 z-10 lg:shadow-primary-web h-[60px] w-full lg:max-w-[75px] lg:min-w-[75px] lg:min-h-[570px] lg:h-screen'>
+    <div className='bg-primary dark:bg-contact-dark-primary h-screen max-h-screen w-full overflow-hidden flex'>
+      <div className='fixed bottom-0 lg:static lg:bottom-none bg-white dark:bg-sidebar-dark-primary border-t border-gray-200 lg:border-0 z-10 lg:shadow-primary-web h-[60px] w-full lg:max-w-[75px] lg:min-w-[75px] lg:min-h-[570px] lg:h-screen'>
         <header></header>
         <nav></nav>
+        <div></div>
       </div>
       <main>
+        <button onClick={handleDarkMode} >dark mode</button>
       </main>
     </div>
   )
--- frontend/linechat web/tailwind.config.js
+++ frontend/linechat web/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -13,6 +14,11 @@ export default {
         'quaternary-blue': '#7269EF',
         'quaternary-light-blue': '#E3E1FC',
         'quaternary-dark-blue': '#4F46E5',
+        'sidebar-dark-primary': '#36404A',
+        'sidebar-dark-btn': '#3E4A56',
+        'contact-dark-primary': '#303841',
+        "chat-dark-primary": "#262E35",
+        'txt-gray': '#EFF2E1',
       },
       boxShadow: {
         'primary-web': '0 2px 4px rgba(15,34,58,.12)',
