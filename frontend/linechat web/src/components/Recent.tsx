d05cbe0829821103c628b4efb72119b6f2d3f284
ilyas <elaissiilyas@gmail.com>
2023-06-13 15:55:12 +0100

    fix issue

--- frontend/linechat web/src/components/Recent.tsx
+++ frontend/linechat web/src/components/Recent.tsx
@@ -2,7 +2,7 @@ import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
 
 const Recent = () => {
-  const calcHeight = 40;
+  
 
   return (
     <div>
@@ -12,7 +12,7 @@ const Recent = () => {
         Recent
       </h5>
       <SimpleBar
-        className={`w-full overflow-y-auto lg:h-[calc(100vh-${calcHeight}px)] h-[calc(100vh-calc(59px+${calcHeight}px))]`}
+        className={`w-full overflow-y-auto lg:h-[calc(100vh-90px)] h-[calc(100vh-calc(59px+90px))]`}
       >
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas harum enim consequatur maxime fugiat natus in sint nobis eius provident ipsum consequuntur quis nemo eum dignissimos culpa, ea ex deserunt ab animi voluptatibus laboriosam cupiditate repellat voluptates. Necessitatibus, expedita ea illo perspiciatis dolore tenetur aliquam eius quasi aut repellat inventore sunt molestias quod quis exercitationem, corporis doloribus voluptates ullam fuga dolor! Expedita libero eligendi voluptatem, laudantium molestias voluptas hic dicta quasi sequi labore optio maiores beatae vero deleniti dolor illo. Inventore error minima quidem, fugit iure consectetur rem corporis, eveniet aliquid sequi voluptates voluptatum expedita optio et ratione aspernatur velit voluptatibus asperiores ipsa aut libero. Sint aliquam dolore officiis minus voluptates asperiores nesciunt delectus ipsa illum facere. Et soluta unde, quisquam exercitationem pariatur suscipit eius sapiente optio consectetur quas, illo, voluptas iusto deleniti quos? Officiis commodi nihil dignissimos id obcaecati placeat, cupiditate itaque? Et molestias incidunt accusantium architecto, adipisci voluptatibus?
