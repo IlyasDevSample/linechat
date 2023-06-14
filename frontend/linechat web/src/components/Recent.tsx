94c5826d6a2ac4095112f36909f9f7925dd1348f
ilyas <elaissiilyas@gmail.com>
2023-06-14 21:05:59 +0100

    ..

--- frontend/linechat web/src/components/Recent.tsx
+++ frontend/linechat web/src/components/Recent.tsx
@@ -1,5 +1,6 @@
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
+import ProfileImg from './ProfileImg';
 
 const Recent = () => {
   
@@ -15,9 +16,13 @@ const Recent = () => {
         className={`w-full overflow-y-auto lg:h-[calc(100vh-267px)] h-[calc(100vh-calc(59px+267px))]`}
       >
         <div
-          className='px-5 py-[15px]'
+          className='px-5 py-[15px] border-t-primary border-t hover:bg-tertiary rounded-md cursor-pointer transition-all'
         >
-
+          <div>
+            <ProfileImg name='ilyas elaissi' />
+          </div>
+          <div></div>
+          <div></div>
         </div>
       </SimpleBar>
     </div>
