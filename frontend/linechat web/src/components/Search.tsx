2611d373bf21f99e361d1fb4f422ef3a2b616b24
ilyas <elaissiilyas@gmail.com>
2023-06-14 15:58:39 +0100

    finishing jsx for the search component

--- frontend/linechat web/src/components/Search.tsx
+++ frontend/linechat web/src/components/Search.tsx
@@ -1,7 +1,33 @@
+import { RiSearch2Line } from "react-icons/ri"
 
-const Search = () => {
+type Props = {
+  placeholder: string
+  title: string
+}
+const Search = ({ placeholder, title }: Props) => {
   return (
-    <div>Search</div>
+    <div
+      className="pt-5 px-6"
+    >
+      <h4
+        className="text-[calc(1.25625rem+.075vw)] font-semibold mb-5 capitalize"
+      >
+        {title}
+      </h4>
+      <div
+        className='flex bg-tertiary dark:bg-sidebar-dark-primary dark:text-dark-blue rounded-md'
+      >
+        <span
+          className="flex items-center justify-center text-xl w-14 text-gray-500 dark:text-dark-blue"
+        >
+          <RiSearch2Line />
+        </span>
+        <input type="text"
+          className="bg-transparent outline-none w-full py-2 px-4 -ml-[1px] text-[.875rem] font-normal text-txt-dark dark:text-txt-dark-primary dark:placeholder:text-dark-blue placeholder:text-sm placeholder:text-txt-gray-2 placeholder:leading-5 h-[45px] dark:text-dark-blue placeholder:capitalize"
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
   )
 }
 
--- frontend/linechat web/src/pages/Chat.tsx
+++ frontend/linechat web/src/pages/Chat.tsx
@@ -9,9 +9,9 @@ const Chat = () => {
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: 100 }}
-      className="w-full text-txt-dark"
+      className="w-full text-txt-dark dark:text-light-gray"
     >
-      <Search />
+      <Search title="chats" placeholder="search recent conversations" />
       <Online />
       <Recent />
     </motion.div>
