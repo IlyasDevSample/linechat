41ae1b0fe9e1187364a33e2dd401b3dc7df46a0c
ilyas <elaissiilyas@gmail.com>
2023-06-14 20:48:37 +0100

    Updating recent page

--- frontend/linechat web/src/components/Recent.tsx
+++ frontend/linechat web/src/components/Recent.tsx
@@ -12,13 +12,13 @@ const Recent = () => {
         Recent
       </h5>
       <SimpleBar
-        className={`w-full overflow-y-auto lg:h-[calc(100vh-90px)] h-[calc(100vh-calc(59px+90px))]`}
+        className={`w-full overflow-y-auto lg:h-[calc(100vh-267px)] h-[calc(100vh-calc(59px+267px))]`}
       >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas harum enim consequatur maxime fugiat natus in sint nobis eius provident ipsum consequuntur quis nemo eum dignissimos culpa, ea ex deserunt ab animi voluptatibus laboriosam cupiditate repellat voluptates. Necessitatibus, expedita ea illo perspiciatis dolore tenetur aliquam eius quasi aut repellat inventore sunt molestias quod quis exercitationem, corporis doloribus voluptates ullam fuga dolor! Expedita libero eligendi voluptatem, laudantium molestias voluptas hic dicta quasi sequi labore optio maiores beatae vero deleniti dolor illo. Inventore error minima quidem, fugit iure consectetur rem corporis, eveniet aliquid sequi voluptates voluptatum expedita optio et ratione aspernatur velit voluptatibus asperiores ipsa aut libero. Sint aliquam dolore officiis minus voluptates asperiores nesciunt delectus ipsa illum facere. Et soluta unde, quisquam exercitationem pariatur suscipit eius sapiente optio consectetur quas, illo, voluptas iusto deleniti quos? Officiis commodi nihil dignissimos id obcaecati placeat, cupiditate itaque? Et molestias incidunt accusantium architecto, adipisci voluptatibus?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas harum enim consequatur maxime fugiat natus in sint nobis eius provident ipsum consequuntur quis nemo eum dignissimos culpa, ea ex deserunt ab animi voluptatibus laboriosam cupiditate repellat voluptates. Necessitatibus, expedita ea illo perspiciatis dolore tenetur aliquam eius quasi aut repellat inventore sunt molestias quod quis exercitationem, corporis doloribus voluptates ullam fuga dolor! Expedita libero eligendi voluptatem, laudantium molestias voluptas hic dicta quasi sequi labore optio maiores beatae vero deleniti dolor illo. Inventore error minima quidem, fugit iure consectetur rem corporis, eveniet aliquid sequi voluptates voluptatum expedita optio et ratione aspernatur velit voluptatibus asperiores ipsa aut libero. Sint aliquam dolore officiis minus voluptates asperiores nesciunt delectus ipsa illum facere. Et soluta unde, quisquam exercitationem pariatur suscipit eius sapiente optio consectetur quas, illo, voluptas iusto deleniti quos? Officiis commodi nihil dignissimos id obcaecati placeat, cupiditate itaque? Et molestias incidunt accusantium architecto, adipisci voluptatibus?
-          end here
-        </p>
+        <div
+          className='px-5 py-[15px]'
+        >
+
+        </div>
       </SimpleBar>
     </div>
   )
