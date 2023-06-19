ee6e6c2ee52625f207e9555ca4819e3c48edb326
ilyas <elaissiilyas@gmail.com>
2023-06-19 15:38:34 +0100

    adding Message Service interface & impl

--- backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/service/message/MessageServiceImpl.java
+++ backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/service/message/MessageServiceImpl.java
@@ -83,7 +83,6 @@ public class MessageServiceImpl implements MessageService {
             userRepository.save(sender);
             userRepository.save(receiver);
         }
-
         return conversation;
     }
 
