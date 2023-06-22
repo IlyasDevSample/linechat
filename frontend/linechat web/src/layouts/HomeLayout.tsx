3adbd3043e74076144f3775c16853badb7d3f79d
ilyas <elaissiilyas@gmail.com>
2023-06-22 19:14:57 +0100

    adding logic to sub and connection to the server

--- frontend/linechat web/src/layouts/HomeLayout.tsx
+++ frontend/linechat web/src/layouts/HomeLayout.tsx
@@ -5,64 +5,57 @@ import SideBar from '../components/SideBar'
 import { Outlet } from 'react-router-dom'
 import UserChat from '../components/UserChat'
 import { AnimatePresence } from 'framer-motion'
-import { useEffect } from 'react'
-import { over } from 'stompjs';
+import { useEffect, useState } from 'react'
+import { Client, over } from 'stompjs';
 import SockJS from 'sockjs-client';
-import { useUserStore } from '../stores/userStore'
 import axios from 'axios'
+import { useUserStore } from '../stores/userStore'
 import { UserDetails } from '../types/userDetailsType'
-import { useNavigate } from 'react-router-dom'
 
 const HomeLayout = () => {
   useTitle()
   useAuthorize()
-  const navigate = useNavigate()
   const bearerToken = useAuthStore((state) => state.bearerToken)
-  const clearBearerToken = useAuthStore((state) => state.clearBearerToken)
+  const [isConnected, setIsConnected] = useState(false)
+  const [client, setClient] = useState<Client>()
   const setUserDetails = useUserStore((state) => state.setUserDetails)
-  const UserDetails = useUserStore((state) => state.UserDetails)
 
   useEffect(() => {
     if (!bearerToken) return;
-
-    axios.get<UserDetails>(import.meta.env.VITE_API_URL + "/profile/details", {
-      headers: {
-        Authorization: bearerToken
-      }
-    })
-      .then((res) => {
-        const userDetails: UserDetails = res.data
-        setUserDetails(userDetails)
-      }).catch((err) => {
-        console.log("error", err);
-        if (err.response?.status === 401) {
-          clearBearerToken()
-          navigate('/login', { replace: true })
+    
+    function onConnect() {
+      setIsConnected(true);
+      axios.get<UserDetails>(import.meta.env.VITE_API_URL+"/profile/details",
+      {
+        headers: {
+          Authorization: bearerToken
         }
+      }).then((response) => {
+        setUserDetails(response.data);
+        client?.subscribe("/topic/user/"+response.data.username, (message) => {
+          console.log("message received");
+        })
+      }).catch((error) => {
+        console.log(error);
       })
+    }
 
-  }, [bearerToken, setUserDetails, clearBearerToken, navigate])
+    function onDisconnect() {
+      setIsConnected(false);
+    }
 
-  useEffect(() => {
-    if (!bearerToken || !UserDetails) return;
-
-    const ws = SockJS(import.meta.env.VITE_SERVER_URL + "/ws?token=" + bearerToken);
+    const ws = SockJS(import.meta.env.VITE_SERVER_URL+"/ws?token=" + bearerToken);
     const client = over(ws);
-    if (client.connected) return;
-    client.connect({}, () => {
-      client.subscribe("/topic/user/" + UserDetails.username, function (message) {
-        console.log(message);
-      })
-    });
+
+    setClient(client);
+
+    client.connect({}, onConnect, onDisconnect);
 
     return () => {
-      try {
-        client.disconnect(() => console.log("Disconnected"));
-      } catch (error) {
-        console.log(error);
-      }
+      client.disconnect(onDisconnect);
     }
-  }, [bearerToken, UserDetails])
+    
+  }, [bearerToken, setUserDetails])
 
   if (!bearerToken) {
     return null
@@ -71,8 +64,16 @@ const HomeLayout = () => {
   return (
     <div className='bg-primary dark:bg-contact-dark-primary h-screen max-h-screen w-full overflow-hidden flex'>
       <SideBar />
+      {isConnected ? <div>Connected</div> : <div>Not Connected</div>}
       <main
         className='flex flex-row w-full overflow-hidden'
+        onClick={() => {
+          client?.send("/app/send", {}, JSON.stringify({
+            sender: "test",
+            receiver: "elaissiilyas@gmail.com",
+            message: "Hello"
+          }));
+        }}
       >
         <div
           className='w-full lg:min-w-[380px] lg:max-w-[380px] h-screen overflow-hidden lg:mr-1'
