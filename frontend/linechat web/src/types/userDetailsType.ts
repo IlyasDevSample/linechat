931d5b732344478332746a3f595fc5cf435d172b
ilyas <elaissiilyas@gmail.com>
2023-06-22 16:54:04 +0100

    user type

---
+++ frontend/linechat web/src/types/userDetailsType.ts
@@ -0,0 +1,10 @@
+export type UserDetails = {
+  username: string;
+  fullName: string;
+  email: string;
+  status: string;
+  lastActive: Date;
+  createdAt: Date;
+  AvatarUrl: null;
+  isEmailVerified: boolean;
+}
