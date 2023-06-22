784f22e94c1302ff1e8b7a07fcda642c018a3301
ilyas <elaissiilyas@gmail.com>
2023-06-22 16:50:47 +0100

    adding route in chat controller

--- backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/controller/ChatController.java
+++ backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/controller/ChatController.java
@@ -3,7 +3,6 @@ package me.ilyaselaissi.linechatapi.controller;
 import me.ilyaselaissi.linechatapi.dto.MessageResponseDTO;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
