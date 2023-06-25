1b08fd056b13ce8439f4edd9839e338009ac571f
ilyas <elaissiilyas@gmail.com>
2023-06-25 12:38:58 +0100

    adding ConversationResponseDTO record

---
+++ backend/LineChatApi/src/main/java/me/ilyaselaissi/linechatapi/dto/ConversationResponseDTO.java
@@ -0,0 +1,15 @@
+package me.ilyaselaissi.linechatapi.dto;
+
+import java.util.List;
+
+public record ConversationResponseDTO(
+        String username,
+        String fullName,
+        String status,
+        String lastActive,
+        String createdAt,
+        String AvatarUrl,
+        List<MessageResponseDTO> messages,
+        String lastMessage
+        ) {
+}
