067d90032319dec2987f83a35df826f0b1e2025c
ilyas <elaissiilyas@gmail.com>
2023-06-27 17:08:24 +0100

    clear state on sign out

--- frontend/linechat web/src/hooks/useSignOut.ts
+++ frontend/linechat web/src/hooks/useSignOut.ts
@@ -2,12 +2,14 @@ import { useAuthStore } from '../stores/authStore'
 import { useNavigate } from 'react-router-dom'
 import { useUserStore } from '../stores/userStore';
 import { useUserSettingStore } from '../stores/userSettingStore';
+import { useConversationStore } from '../stores/conversationStore';
 
 export const useSignOut = () => {
   const clearBearerToken = useAuthStore(state => state.clearBearerToken);
   const clearUserDetails = useUserStore((state) => state.clearUserDetails)
   const setIsOnline = useUserStore((state) => state.setIsOnline)
   const setDarkMode = useUserSettingStore((state) => state.setDarkMode)
+  const clearConversations = useConversationStore((state) => state.clearConversations)
   const navigate = useNavigate();
 
   const signOut = (redirectTo = '/') => {
@@ -15,6 +17,7 @@ export const useSignOut = () => {
     clearUserDetails();
     setIsOnline(false);
     setDarkMode(false);
+    clearConversations();
     navigate(redirectTo);
   }
 
